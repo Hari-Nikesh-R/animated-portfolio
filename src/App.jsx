cfde873804e9e850d81852ee61cd0285610001c7
Hari-Nikesh-R <hari.nikesh.r.cce@gmail.com>
2024-11-02 23:51:00 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import tailwindConfig from '../tailwind.config';
+const App = () => {
+  return (
+    <main className="max-w-7xl mx-auto">
+    <h1 className="text-2xl text-white underline">Hello Three js</h1>
+    </main>
+  )
+}
+
+
+export default App;
